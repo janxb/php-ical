84039f852f567a2aab0f785f64ad375e91470c45
Jan Brodda <git@janbrodda.de>
2018-10-26 11:14:50 +0200

    moved dependency update to separate method

--- bin/update-web-dependencies.py
+++ bin/update-web-dependencies.py
@@ -3,11 +3,17 @@
 import sys,re,urllib.request,json,fileinput
 from prettytable import PrettyTable
 
-def replaceAll(file,searchExp,replaceExp):
+def replaceLinesInFile(file,searchExp,replaceExp):
     for line in fileinput.input(file, inplace=1):
         if searchExp in line:
             line = line.replace(searchExp,replaceExp)
         sys.stdout.write(line)
+
+def updateDependency(name, oldVersion, newVersion, line, filename, updates):
+	if oldVersion != newVersion:
+		updatedLine = line.replace(oldVersion,newVersion)
+		replaceLinesInFile(filename, line, updatedLine)
+		updates.add_row([name, oldVersion, newVersion])
 
 filename = sys.argv[1];
 updates = PrettyTable(['Dependency', 'Old Version', 'New Version'])
@@ -19,11 +25,7 @@ for line in open(filename).read().split("\n"):
 			dep_version = dependency.groups()[1];
 			with urllib.request.urlopen("https://api.cdnjs.com/libraries/"+dep_name+"?fields=name,version") as url:
 				data = json.loads(url.read().decode())
-				dep_version_new = data["version"]
-				if dep_version != dep_version_new:
-					updatedLine = line.replace(dep_version,dep_version_new)
-					replaceAll(filename, line, updatedLine)
-					updates.add_row([dep_name, dep_version, dep_version_new])
+				updateDependency(dep_name, dep_version, data["version"], line, filename, updates)
 
 	if "use.fontawesome" in line:
 		dependency = re.match("(?:.+)\/releases\/([0-9a-zA-Z\.\-]+)\/", line);
@@ -32,10 +34,6 @@ for line in open(filename).read().split("\n"):
 			dep_version = dependency.groups()[0];
 			with urllib.request.urlopen("https://api.github.com/repos/FortAwesome/Font-Awesome/releases/latest") as url:
 				data = json.loads(url.read().decode())
-				dep_version_new = "v"+data["tag_name"]
-				if dep_version != dep_version_new:
-					updatedLine = line.replace(dep_version,dep_version_new)
-					replaceAll(filename, line, updatedLine)
-					updates.add_row([dep_name, dep_version, dep_version_new])
+				updateDependency(dep_name, dep_version, "v"+data["tag_name"], line, filename, updates)
 		
 print(updates)
